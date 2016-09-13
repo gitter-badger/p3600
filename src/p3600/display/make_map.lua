a49eb411eb1ef5b469eb714687158e76fbb5bf80
Christopher Clements <bcnjr5@gmail.com>
2016-09-13 14:58:30 -0400

    Re-added entrances

--- src/p3600/display/make_map.lua
+++ src/p3600/display/make_map.lua
@@ -4,6 +4,7 @@ return function(mapdata)
     height = mapdata.layers['tiletypes'].height,
     tiletype = {},
     data = mapdata,
+    entrances = {},
     exits = {},
     vx = 0,
     vy = 0,
@@ -22,6 +23,23 @@ return function(mapdata)
     map.exits['bottom'] = e['bottom']
     map.exits['left'] = e['left']
     map.exits['right'] = e['right']
+  end
+
+  if not (mapdata.layers['entrances'] == nil) then
+    for _, e in pairs(mapdata.layers['entrances'].objects) do
+      local id = e.name:gmatch('[^/]+')()
+      local subv = e.name:gsub('.*/', '')
+
+      if (map.entrances[id] == nil) then
+        map.entrances[id] = {}
+      end
+
+      map.entrances[id][subv] = {
+        x = e.x / 16,
+        y = e.y / 16,
+        dir = e.properties.dir,
+      }
+    end
   end
 
   return map
